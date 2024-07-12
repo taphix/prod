3351b953082076688c8c0d3ba092dd6b9b57c711
taphix <taphix1@gmail.com>
2024-07-12 14:03:53 +0300

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/taphix/prod.git"
+REPO_DIR="prod"
+
+update_repo() {
+    if [ -d "$REPO_DIR" ]; then
+        cd "$REPO_DIR"
+        git pull origin main
+        cd ..
+    else
+        git clone "$REPO_URL"
+    fi
+}
+
+stop_containers() {
+    if [ -d "$REPO_DIR" ]; then
+        cd "$REPO_DIR"
+        docker compose down
+        cd ..
+    fi
+}
+
+build_and_run_containers() {
+    if [ -d "$REPO_DIR" ]; then
+        cd "$REPO_DIR"
+        docker compose up --build -d
+        cd ..
+    fi
+}
+
+update_repo
+stop_containers
+build_and_run_containers
+
+echo "Deployment complete."
