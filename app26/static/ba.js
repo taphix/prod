ef64bb5848545f8ae4d6159211290eee2d11395a
Roma <biosice@gmail.com>
2024-08-09 14:20:01 +1000

    update x400pro.club

---
+++ app26/static/ba.js
@@ -0,0 +1,60 @@
+const url = new URL(window.location.href);
+const keywordValue = url.searchParams.get("keyword");
+
+       cookieDialog.style.opacity = '0';
+        cookieDialog.style.visibility = 'hidden';
+
+if (!keywordValue || !/avie|avia/.test(keywordValue)) {
+    // window.location.href = '/site';
+    document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.bg-banner').style.display = 'none';
+    document.body.style.overflow = 'auto';
+    document.documentElement.style.overflow = 'auto';
+
+ cookieDialog.style.opacity = '1';
+        cookieDialog.style.visibility = 'visible';
+} else {
+    if (location.search) {
+        localStorage.setItem('savedQuery', location.search);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeBtn = document.getElementById('closeBtn');
+    const banner = document.getElementById('banner');
+    let countdown = 10;
+
+    const countdownInterval = setInterval(function () {
+        countdown--;
+        if (countdown <= 0) {
+            clearInterval(countdownInterval);
+            closeBtn.textContent = '×';
+            closeBtn.classList.add('active');
+            closeBtn.style.cursor = 'pointer';
+        } else {
+            closeBtn.textContent = countdown;
+        }
+    }, 1000);
+
+    closeBtn.addEventListener('click', function (event) {
+        event.stopPropagation();
+        if (closeBtn.classList.contains('active')) {
+            // window.location.href = '/site';
+            document.querySelector('.banner').style.display = 'none';
+            document.querySelector('.bg-banner').style.display = 'none';
+            document.body.style.overflow = 'auto';
+            document.documentElement.style.overflow = 'auto';
+
+            cookieDialog.style.opacity = '1';
+        cookieDialog.style.visibility = 'visible';
+        }
+    });
+
+    banner.addEventListener('click', function (event) {
+        if (!event.target.closest('.close-btn')) {
+            const savedQuery = localStorage.getItem('savedQuery');
+            const redirectUrl = `https://aviacompassupr.top/XM7BRh${savedQuery ? savedQuery : ''}`;
+            window.location.href = redirectUrl;
+        }
+    });
+});
