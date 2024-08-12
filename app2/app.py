fd24d07308d86f1ee0322cfbfdc69b581cb3d043
taphix <taphix1@gmail.com>
2024-08-12 13:14:41 +0300

    new clo

--- app2/app.py
+++ app2/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
@@ -6,41 +7,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/privacy')
-def privacy():
-    return render_template('privacy.html')
-    
-@app.route('/terms')
-def terms():
-    return render_template('terms.html')
-    
-@app.route('/dmca')
-def dmca():
-    return render_template('dmca.html')
-    
-@app.route('/data')
-def data():
-    return render_template('data.html')
-    
-@app.route('/cookie')
-def cookie():
-    return render_template('cookie.html')
-
 @app.route('/main')
 def main():
     return render_template('main.html')
 
-@app.route('/pwa')
-def pwa():
-    return render_template('pwa.html')
-
-@app.route('/manifest.json')
-def serve_manifest():
-    return send_from_directory('static/', 'manifest.json')
-
-@app.route('/sw.js')
-def serve_sw():
-    return send_from_directory('static/', 'sw.js')
+@app.route('/process')
+def process():
+    # Получение IP и User-Agent с использованием Flask
+    ip = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    keyword_value = request.args.get("keyword")
+    
+    # Подготовка данных для API-запроса
+    token = "pxtshqzgkd5p7fvkrm4rqtcv2cz7czyq"
+    api_url = f"https://flyingbalance.icu/click_api/v3?token={token}&log=1&info=1&ip={ip}&user_agent={user_agent}&keyword={keyword_value}"
+    
+    # Выполнение запроса к API
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if data.get('info', {}).get('url') == 'OFFER':
+            return redirect(f"/main?keyword={keyword_value}")
+        elif data.get('info', {}).get('url') == 'BOT':
+            # Логика обработки BOT'а
+            print("BOT detected")
+    
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8000')
