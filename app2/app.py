9ae4c126ba74958c5095ec68f2b4d9454912d577
taphix <taphix1@gmail.com>
2024-08-13 16:16:58 +0300

    update clo

--- app2/app.py
+++ app2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import requests
 
 app = Flask(__name__)
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def index():
     ip = request.remote_addr
     user_agent = request.headers.get('User-Agent')
-    keyword_value = request.args.get("keyword")
+    params = request.args
+    keyword_value = params.get("keyword")
     
     if not keyword_value:
         return render_template('index.html')
@@ -17,21 +18,54 @@ def index():
     
     response = requests.get(api_url)
     print(response)
-    
+
     if response.status_code == 200:
         data = response.json()
-        print(data)
-        if data.get('info', {}).get('url') == 'OFFER':
-            return redirect(url_for('main'))
-        elif data.get('info', {}).get('url') == 'BOT':
+        print(data.get('info', {}).get('url'))
+        if data.get('info', {}).get('url') == 'do5WKVd4':
+            return redirect(url_for('main', **params))
+        else:
             return render_template('index.html', message="BOT", keyword=keyword_value, ip=ip)
     
     return render_template('index.html')
 
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+    
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+    
+@app.route('/dmca')
+def dmca():
+    return render_template('dmca.html')
+    
+@app.route('/data')
+def data():
+    return render_template('data.html')
+    
+@app.route('/cookie')
+def cookie():
+    return render_template('cookie.html')
+
 @app.route('/main')
 def main():
-    keyword_value = request.args.get("keyword")
-    return render_template('main.html', keyword=keyword_value)
+    return render_template('main.html')
+
+@app.route('/pwa')
+def pwa():
+    return render_template('pwa.html')
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_from_directory('static/', 'manifest.json')
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_from_directory('static/', 'sw.js')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
