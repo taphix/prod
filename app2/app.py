e45e60f850af759727ee0c0110200a5bc6284293
taphix <taphix1@gmail.com>
2024-08-13 15:50:22 +0300

    update clo

--- app2/app.py
+++ app2/app.py
@@ -1,39 +1,36 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/main')
-def main():
-    return render_template('main.html')
-
-@app.route('/process')
-def process():
-    # Получение IP и User-Agent с использованием Flask
     ip = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     keyword_value = request.args.get("keyword")
     
-    # Подготовка данных для API-запроса
+    if not keyword_value:
+        return render_template('index.html')
+
     token = "pxtshqzgkd5p7fvkrm4rqtcv2cz7czyq"
     api_url = f"https://flyingbalance.icu/click_api/v3?token={token}&log=1&info=1&ip={ip}&user_agent={user_agent}&keyword={keyword_value}"
     
-    # Выполнение запроса к API
     response = requests.get(api_url)
     
     if response.status_code == 200:
         data = response.json()
+        print(data)
         if data.get('info', {}).get('url') == 'OFFER':
-            return redirect(f"/main?keyword={keyword_value}")
+            return redirect(url_for('main'))
         elif data.get('info', {}).get('url') == 'BOT':
-            # Логика обработки BOT'а
-            print("BOT detected")
+            return render_template('index.html', message="BOT", keyword=keyword_value, ip=ip)
     
     return render_template('index.html')
 
+@app.route('/main')
+def main():
+    keyword_value = request.args.get("keyword")
+    return render_template('main.html', keyword=keyword_value)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8000')
+    app.run(host='0.0.0.0', port=8000)
