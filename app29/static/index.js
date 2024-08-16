00c33ae6f7c1596db14e1a37289e417ce647700f
taphix <taphix1@gmail.com>
2024-08-16 18:09:12 +0300

    add banner

---
+++ app29/static/index.js
@@ -0,0 +1,24 @@
+// ---- ---- Const ---- ---- //
+const cookiesBox = document.querySelector('.cookies'),
+    buttons = document.querySelectorAll('.button');
+
+// ---- ---- Show ---- ---- //
+const executeCodes = () => {
+   // if (document.cookie.includes('aviator')) return;
+  //  cookiesBox.classList.add('show');
+
+    // ---- ---- Button ---- ---- //
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            cookiesBox.classList.remove('show');
+
+            // ---- ---- Time ---- ---- //
+            // if (button.id == 'acceptBtn') {
+            //     document.cookie =
+            //         'cookieBy= aviator; max-age=' + 60 * 60 * 24 * 30;
+            // }
+        });
+    });
+};
+
+window.addEventListener('load', executeCodes);
